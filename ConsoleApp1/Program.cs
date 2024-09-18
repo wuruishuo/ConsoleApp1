65674468f0c9c20250e5781261ce29b11f9201e5
wuruishuo <3179544063@qq.com>
2024-09-18 20:42:18 +0800

    git@github.com:wuruishuo/test.git

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("sdfs");
+Console.ReadLine();
+Console.SetWindowSize(100,200000);
+Console.SetBufferSize(1000,10000000);
